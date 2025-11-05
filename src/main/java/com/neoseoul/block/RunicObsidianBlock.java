9f9bce558458b0a57303b02222e02307fd18d1fa
C3faa <crytotik5@gmail.com>
2025-11-05 20:52:29 +0300

    Update RunicObsidianBlock.java

--- src/main/java/com/neoseoul/block/RunicObsidianBlock.java
+++ src/main/java/com/neoseoul/block/RunicObsidianBlock.java
@@ -4,6 +4,7 @@ import com.neoseoul.rift.RiftManager;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -11,14 +12,16 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class RunicObsidianBlock extends Block {
+
     public RunicObsidianBlock(Settings settings) {
         super(settings);
     }
 
     @Override
+    @SuppressWarnings("deprecation")
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (!world.isClient) {
-            RiftManager.get(world.getServer()).onBlockActivated(world, pos, player);
+            RiftManager.get(world.getServer()).onBlockActivated((ServerWorld) world, pos, player);
         }
         return ActionResult.SUCCESS;
     }
