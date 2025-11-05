606334a4101066569d1b757b5e64787e469d912d
C3faa <crytotik5@gmail.com>
2025-11-05 23:00:06 +0300

    Update ModBlocks.java

--- src/main/java/com/neoseoul/block/ModBlocks.java
+++ src/main/java/com/neoseoul/block/ModBlocks.java
@@ -1,32 +1,37 @@
 package com.neoseoul.block;
 
 import com.neoseoul.NeoriftsMod;
-import net.minecraft.block.AbstractBlock;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public final class ModBlocks {
-
-    // Сам блок рифта
-    public static final Block RUNIC_OBSIDIAN =
-            new RunicObsidianBlock(AbstractBlock.Settings.copy(Blocks.OBSIDIAN).nonOpaque());
-
-    private ModBlocks() {}
+    public static final RunicObsidianBlock RUNIC_OBSIDIAN = new RunicObsidianBlock(
+            AbstractBlock.Settings.create().strength(50.0f, 1200.0f).requiresTool()
+    );
 
     public static void register() {
-        // Регистрируем блок
+        // Блок
         Registry.register(Registries.BLOCK,
                 new Identifier(NeoriftsMod.MOD_ID, "runic_obsidian"),
                 RUNIC_OBSIDIAN);
 
-        // Регистрируем предмет-блок
+        // Айтем блока
         Registry.register(Registries.ITEM,
                 new Identifier(NeoriftsMod.MOD_ID, "runic_obsidian"),
                 new BlockItem(RUNIC_OBSIDIAN, new Item.Settings()));
+
+        // Вкладка креатива
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
+            entries.add(RUNIC_OBSIDIAN.asItem());
+        });
     }
+
+    private ModBlocks() {}
 }
